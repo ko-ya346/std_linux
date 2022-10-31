7ea6a6da0ad351a4168aee46e9a519afa7c8c032
kouya takahashi <3kamarin4skaya6@gmail.com>
2022-10-31 09:49:58 +0900

    add comment

--- src/cat.c
+++ src/cat.c
@@ -10,6 +10,9 @@ main(int argc, char *argv[])
         FILE *f;
         int c;
 
+        /* fopen: stdioにおいてopen()に対応するAPI
+         * ファイルをパスでしていすると、そのファイルにつながるストリームを作り、
+         * それを管理するFILE型へのポインタを返す*/
         f = fopen(argv[i], "r");
         /* !f 否定*/
         if (!f) {
@@ -17,7 +20,6 @@ main(int argc, char *argv[])
             exit(1);
         }
         /*fgetc: 引数のstreamから1 byte読み込んで返す。streamが終了したらEOF(-1)を返す*/
-        /*fputc: 引数のstreamにバイトcを書き込む*/
         while ((c = fgetc(f)) != EOF) {
                 if (putchar(c) < 0) exit(1);
         }
